83328c6c17c1d8dca5e14e1c1b1320655d89dbc7
Görkem Kaya <gorkemkayas@hotmail.com>
2024-12-09 18:36:09 +0300

    Role Ekleme kısmı eklendi, client validasyon eklendi.

--- deneme/Models/Role.cs
+++ deneme/Models/Role.cs
@@ -1,8 +1,12 @@
-﻿namespace deneme.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace deneme.Models
 {
     public class Role
     {
         public int RoleId { get; set; }
+
+        [Required]
         public string RoleName { get; set; } = null!;
 
     }
---
+++ deneme/Pages/RoleAdd.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace deneme.Pages
+{
+    public class RoleAddModel : PageModel
+    {
+        private string? _connectionString;
+
+        [BindProperty]
+        public string RoleName { get; set; } = null!;
+        public RoleAddModel(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MSSQL");
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                AddRoleToDatabase(RoleName);
+                return RedirectToPage("/Roles");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        private void AddRoleToDatabase(string roleName)
+        {
+
+            string query = $"INSERT INTO ROLE (RoleName) VALUES (@RoleName)";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@RoleName", roleName);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("AddRole metodunda hata oluştu.");
+                }
+            }
+        }
+    }
+}
